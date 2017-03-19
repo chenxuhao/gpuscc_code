fb675405a0f9ce54925c2bdabc11432a9285722e
chenxuhao <cxh@illinois.edu>
2017-03-19 20:17:13 +0800

    add topo

--- reach_b.cu
+++ reach_b.cu
@@ -410,7 +410,7 @@ static __global__ void bwd_step1_td(unsigned *nodes, unsigned *edges, unsigned c
 	}
 }
 
-static __global__ void fwd_step1_topo(unsigned *nodes, unsigned *edges, unsigned char *states, unsigned countOfNodes, volatile bool *end) {
+static __global__ void fwd_step1_topo(unsigned *nodes, unsigned *edges, unsigned char *states, unsigned countOfNodes, int *scout_count, int *degree, volatile bool *end) {
 	unsigned id = blockDim.x * blockIdx.x + threadIdx.x + 1;
 	if (id > countOfNodes) return;
 	if((states[id] & 70) == 2) { // if it is visited but not expanded
@@ -421,7 +421,26 @@ static __global__ void fwd_step1_topo(unsigned *nodes, unsigned *edges, unsigned
 			unsigned dst = edges[i];
 			if((states[dst] & 6) == 0) { // if it is not visited
 				setFVis(&states[dst]);
-				*end = false;
+				scout_count[dst] = degree[dst];
+				//*end = false;
+			}
+		}
+	}
+}
+
+static __global__ void bwd_step1_topo(unsigned *nodes, unsigned *edges, unsigned char *states, unsigned countOfNodes, int *scout_count, int *degree, volatile bool *end) {
+	unsigned id = blockDim.x * blockIdx.x + threadIdx.x + 1;
+	if (id > countOfNodes) return;
+	if((states[id] & 37) == 1) { // if it is visited but not expanded
+		setBExt(&states[id]);
+		unsigned my_start = nodes[id];
+		unsigned my_end = nodes[id + 1];
+		for(unsigned i = my_start; i < my_end; i++) {
+			unsigned dst = edges[i];
+			if((states[dst] & 5) == 0) { // if it is not visited
+				setBVis(&states[dst]);
+				scout_count[dst] = degree[dst];
+				//*end = false;
 			}
 		}
 	}
@@ -482,6 +501,19 @@ __global__ void BitmapToQueue(int countOfNodes, int *bm, Worklist2 queue) {
 	if(bm[id]) queue.push(id);
 }
 
+__global__ void FindFwdQueue(int countOfNodes, unsigned char *states, int *bm) {
+	unsigned id = blockIdx.x * blockDim.x + threadIdx.x + 1;
+	if(id > countOfNodes) return;
+	if((states[id] & 70) == 2) bm[id] = 1;
+	else bm[id] = 0;
+}
+
+__global__ void FindBwdQueue(int countOfNodes, unsigned char *states, int *bm) {
+	unsigned id = blockIdx.x * blockDim.x + threadIdx.x + 1;
+	if(id > countOfNodes) return;
+	if((states[id] & 37) == 1) bm[id] = 1;
+	else bm[id] = 0;
+}
 
 __global__ void insert(int source, Worklist2 queue) {
 	int id = blockIdx.x * blockDim.x + threadIdx.x;
@@ -489,16 +521,20 @@ __global__ void insert(int source, Worklist2 queue) {
 	return;
 }
 ///*
+#define TD
 //hybrid BFS only for phase-1 (to identify the single giant SCC)
 unsigned fwd_reach1_b(unsigned *d_nodes, unsigned *d_nodesT, unsigned countOfNodes, unsigned *d_edges, unsigned *d_edgesT, unsigned countOfEdges, unsigned char *d_states, alg_opt opt, int *h_degree, int source) {
-	//bool volatile *d_end;
-	//bool end = false;
-	unsigned depth = 0;
 	dim3 dimGrid;
-	//gpuErrchk( cudaMalloc((void**)&d_end, sizeof(*d_end)) );
-
+	unsigned depth = 0;
+#ifdef TD
 	Worklist2 queue1(countOfEdges), queue2(countOfEdges);
 	Worklist2 *in_frontier = &queue1, *out_frontier = &queue2;
+	insert<<<1, 32>>>(source, *in_frontier);
+#else
+	//bool volatile *d_end;
+	//bool end = false;
+	//gpuErrchk( cudaMalloc((void**)&d_end, sizeof(*d_end)) );
+#endif
 	int *front, *next;
 	int zero = 0;
 	int alpha = 15, beta = 18;
@@ -507,7 +543,11 @@ unsigned fwd_reach1_b(unsigned *d_nodes, unsigned *d_nodesT, unsigned countOfNod
 	int scout_count = h_degree[source];
 	int *d_scout_count, *d_frontier_size;
 	int *d_degree;
+#ifdef TD
 	CUDA_SAFE_CALL(cudaMalloc((void **)&d_scout_count, sizeof(int)));
+#else
+	CUDA_SAFE_CALL(cudaMalloc((void **)&d_scout_count, (countOfNodes+1) * sizeof(int)));
+#endif
 	CUDA_SAFE_CALL(cudaMalloc((void **)&d_frontier_size, sizeof(int)));
 	CUDA_SAFE_CALL(cudaMalloc((void **)&d_degree, (countOfNodes+1) * sizeof(int)));
 	CUDA_SAFE_CALL(cudaMemcpy(d_degree, h_degree, (countOfNodes+1) * sizeof(int), cudaMemcpyHostToDevice));
@@ -515,13 +555,16 @@ unsigned fwd_reach1_b(unsigned *d_nodes, unsigned *d_nodesT, unsigned countOfNod
 	CUDA_SAFE_CALL(cudaMalloc((void **)&next, (countOfNodes+1) * sizeof(int)));
 	CUDA_SAFE_CALL(cudaMemset(front, 0, (countOfNodes+1) * sizeof(int)));
 	CUDA_SAFE_CALL(cudaMemset(next, 0, (countOfNodes+1) * sizeof(int)));
-	insert<<<1, 32>>>(source, *in_frontier);
 
 	do {
 		if(scout_count > edges_to_check / alpha) {
-		//if(1) {
+		//if(0) {
 			int awake_count, old_awake_count;
+#ifdef TD
 			QueueToBitmap<<<((countOfNodes-1)/512+1), 512>>>(countOfNodes, *in_frontier, front);
+#else
+			FindFwdQueue<<<((countOfNodes-1)/512+1), 512>>>(countOfNodes, d_states, front);
+#endif
 			awake_count = nitems;
 			do {
 				depth++;
@@ -531,7 +574,7 @@ unsigned fwd_reach1_b(unsigned *d_nodes, unsigned *d_nodesT, unsigned countOfNod
 				fwd_step1_bu<<<dimGrid, opt.block_size>>>(d_nodesT, d_edgesT, d_states, countOfNodes, front, next, d_frontier_size);
 				//gpuErrchk( cudaMemcpy(&awake_count, (void *)d_frontier_size, sizeof(int), cudaMemcpyDeviceToHost) );
 				awake_count = thrust::reduce(thrust::device, next + 1, next + countOfNodes + 1, 0, thrust::plus<int>());
-				printf("BU: iteration=%d, num_frontier=%d\n", depth, awake_count);
+				//printf("BU: iteration=%d, num_frontier=%d\n", depth, awake_count);
 				int *temp = front;
 				front = next;
 				next = temp;
@@ -540,11 +583,14 @@ unsigned fwd_reach1_b(unsigned *d_nodes, unsigned *d_nodesT, unsigned countOfNod
 			//} while(awake_count>0);
 			nitems = awake_count;
 			scout_count = 1;
+#ifdef TD
 			in_frontier->clearHost();
 			BitmapToQueue<<<((countOfNodes-1)/512+1), 512>>>(countOfNodes, front, *in_frontier);
+#endif
 		} else{ 
 			depth++;
 			edges_to_check -= scout_count;
+#ifdef TD
 			nitems = in_frontier->getSize();
 			setGridDimension(&dimGrid, nitems, opt.block_size);
 			CUDA_SAFE_CALL(cudaMemcpy(d_scout_count, &zero, sizeof(int), cudaMemcpyHostToDevice));
@@ -557,22 +603,43 @@ unsigned fwd_reach1_b(unsigned *d_nodes, unsigned *d_nodesT, unsigned countOfNod
 			out_frontier = tmp;
 			//out_frontier->reset();
 			out_frontier->clearHost();
-			printf("TD: iteration=%d, num_frontier=%d\n", depth, nitems);
+			//printf("TD: iteration=%d, num_frontier=%d\n", depth, nitems);
+#else
+			setGridDimension(&dimGrid, countOfNodes, opt.block_size);
+			//gpuErrchk( cudaMemset((void *)d_end, true, sizeof(*d_end)) );
+			//CUDA_SAFE_CALL(cudaMemcpy(d_scout_count, &zero, sizeof(int), cudaMemcpyHostToDevice));
+			thrust::fill(thrust::device, d_scout_count, d_scout_count + countOfNodes + 1, 0);
+			fwd_step1_topo<<<dimGrid, BLKSIZE>>>(d_nodes, d_edges, d_states, countOfNodes, d_scout_count, d_degree, NULL);
+			//CUDA_SAFE_CALL(cudaMemcpy(&scout_count, d_scout_count, sizeof(int), cudaMemcpyDeviceToHost));
+			scout_count = thrust::reduce(thrust::device, d_scout_count, d_scout_count + countOfNodes + 1, 0, thrust::plus<int>());
+			//gpuErrchk( cudaMemcpy(&end, (void *)d_end, sizeof(*d_end), cudaMemcpyDeviceToHost) );
+			//if(end) nitems = 0;
+			if(scout_count == 0) nitems = 0;
+			//printf("TOPO: iteration=%d, scout_count=%d\n", depth, scout_count);
+#endif
 		}
 	} while (nitems > 0);
 	gpuErrchk(cudaDeviceSynchronize());
 	gpuErrchk(cudaFree(d_scout_count));
+	gpuErrchk(cudaFree(d_degree));
+	gpuErrchk(cudaFree(front));
+	gpuErrchk(cudaFree(next));
+	gpuErrchk(cudaFree(d_frontier_size));
 	//gpuErrchk( cudaFree((void *) d_end) );
 	return depth;
 }
 //*/
 ///*
+#define BW_TD
 unsigned bwd_reach1_b(unsigned *d_nodes, unsigned *d_nodesT, unsigned countOfNodes, unsigned *d_edges, unsigned *d_edgesT, unsigned countOfEdges, unsigned char *d_states, alg_opt opt, int *h_degree, int source) {
 	unsigned depth = 0;
 	dim3 dimGrid;
-
+#ifdef BW_TD
 	Worklist2 queue1(countOfEdges), queue2(countOfEdges);
 	Worklist2 *in_frontier = &queue1, *out_frontier = &queue2;
+	insert<<<1, 32>>>(source, *in_frontier);
+#endif
+	
 	int *front, *next;
 	int zero = 0;
 	int alpha = 15, beta = 18;
@@ -581,7 +648,11 @@ unsigned bwd_reach1_b(unsigned *d_nodes, unsigned *d_nodesT, unsigned countOfNod
 	int scout_count = h_degree[source];
 	int *d_scout_count, *d_frontier_size;
 	int *d_degree;
+#ifdef BW_TD
 	CUDA_SAFE_CALL(cudaMalloc((void **)&d_scout_count, sizeof(int)));
+#else
+	CUDA_SAFE_CALL(cudaMalloc((void **)&d_scout_count, (countOfNodes+1) * sizeof(int)));
+#endif
 	CUDA_SAFE_CALL(cudaMalloc((void **)&d_frontier_size, sizeof(int)));
 	CUDA_SAFE_CALL(cudaMalloc((void **)&d_degree, (countOfNodes+1) * sizeof(int)));
 	CUDA_SAFE_CALL(cudaMemcpy(d_degree, h_degree, (countOfNodes+1) * sizeof(int), cudaMemcpyHostToDevice));
@@ -589,13 +660,16 @@ unsigned bwd_reach1_b(unsigned *d_nodes, unsigned *d_nodesT, unsigned countOfNod
 	CUDA_SAFE_CALL(cudaMalloc((void **)&next, (countOfNodes+1) * sizeof(int)));
 	CUDA_SAFE_CALL(cudaMemset(front, 0, (countOfNodes+1) * sizeof(int)));
 	CUDA_SAFE_CALL(cudaMemset(next, 0, (countOfNodes+1) * sizeof(int)));
-	insert<<<1, 32>>>(source, *in_frontier);
 
 	do {
 		if(scout_count > edges_to_check / alpha) {
 		//if(1) {
 			int awake_count, old_awake_count;
+#ifdef BW_TD
 			QueueToBitmap<<<((countOfNodes-1)/512+1), 512>>>(countOfNodes, *in_frontier, front);
+#else
+			FindBwdQueue<<<((countOfNodes-1)/512+1), 512>>>(countOfNodes, d_states, front);
+#endif
 			awake_count = nitems;
 			do {
 				depth++;
@@ -605,36 +679,48 @@ unsigned bwd_reach1_b(unsigned *d_nodes, unsigned *d_nodesT, unsigned countOfNod
 				bwd_step1_bu<<<dimGrid, opt.block_size>>>(d_nodes, d_edges, d_states, countOfNodes, front, next, d_frontier_size);
 				//gpuErrchk( cudaMemcpy(&awake_count, (void *)d_frontier_size, sizeof(int), cudaMemcpyDeviceToHost) );
 				awake_count = thrust::reduce(thrust::device, next + 1, next + countOfNodes + 1, 0, thrust::plus<int>());
-				printf("BU: iteration=%d, num_frontier=%d\n", depth, awake_count);
+				//printf("BU: iteration=%d, num_frontier=%d\n", depth, awake_count);
 				int *temp = front;
 				front = next;
 				next = temp;
 				thrust::fill(thrust::device, next, next + countOfNodes + 1, 0);
 			} while((awake_count >= old_awake_count) || (awake_count > countOfNodes / beta));
-			//} while(awake_count>0);
 			nitems = awake_count;
 			scout_count = 1;
+#ifdef BW_TD
 			in_frontier->clearHost();
 			BitmapToQueue<<<((countOfNodes-1)/512+1), 512>>>(countOfNodes, front, *in_frontier);
+#endif
 		} else{ 
 			depth++;
 			edges_to_check -= scout_count;
+#ifdef BW_TD
 			nitems = in_frontier->getSize();
 			setGridDimension(&dimGrid, nitems, opt.block_size);
 			CUDA_SAFE_CALL(cudaMemcpy(d_scout_count, &zero, sizeof(int), cudaMemcpyHostToDevice));
 			bwd_step1_td<<<dimGrid, BLKSIZE>>>(d_nodesT, d_edgesT, d_states, countOfNodes, d_scout_count, d_degree, *in_frontier, *out_frontier);
 			CUDA_SAFE_CALL(cudaMemcpy(&scout_count, d_scout_count, sizeof(int), cudaMemcpyDeviceToHost));
-			//nitems = out_frontier->nitems();
 			nitems = out_frontier->getSize();
 			Worklist2 *tmp = in_frontier;
 			in_frontier = out_frontier;
 			out_frontier = tmp;
-			//out_frontier->reset();
 			out_frontier->clearHost();
-			printf("TD: iteration=%d, num_frontier=%d\n", depth, nitems);
+			//printf("TD: iteration=%d, num_frontier=%d\n", depth, nitems);
+#else
+			setGridDimension(&dimGrid, countOfNodes, opt.block_size);
+			thrust::fill(thrust::device, d_scout_count, d_scout_count + countOfNodes + 1, 0);
+			bwd_step1_topo<<<dimGrid, BLKSIZE>>>(d_nodesT, d_edgesT, d_states, countOfNodes, d_scout_count, d_degree, NULL);
+			scout_count = thrust::reduce(thrust::device, d_scout_count, d_scout_count + countOfNodes + 1, 0, thrust::plus<int>());
+			if(scout_count == 0) nitems = 0;
+			//printf("TOPO: iteration=%d, scout_count=%d\n", depth, scout_count);
+#endif
 		}
 	} while (nitems > 0);
 	gpuErrchk(cudaFree(d_scout_count));
+	gpuErrchk(cudaFree(d_degree));
+	gpuErrchk(cudaFree(front));
+	gpuErrchk(cudaFree(next));
+	gpuErrchk(cudaFree(d_frontier_size));
 	return depth;
 }
 //*/
